b6239e1429a481029d6fe373d6dd515f21c2d981
Igor Albuquerque <igoralbuquerque102@hotmail.com>
2024-09-26 15:01:56 -0300

    versao com o banco

--- docker/todolist/__init__.py
+++ docker/todolist/__init__.py
@@ -1,56 +1,59 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='templates')
 
 # Ler a variável de ambiente para o endpoint do RDS
 rds_endpoint = os.getenv("END_POINT")
 
-# Configurar o banco de dados PostgreSQL (comentado para teste)
-#app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://db_admin:password123@{rds_endpoint}:5432/todolist_db'
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Configurar o banco de dados PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://db_admin:password123@{rds_endpoint}:5432/todolist_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Inicializar a conexão com o banco de dados (comentado para teste)
-#db = SQLAlchemy(app)
-db = None  # Definir db como None para não interagir com o banco de dados
+# Inicializar a conexão com o banco de dados
+db = SQLAlchemy(app)
 
-# Modelo Todo (comentado para não gerar erro)
-# class Todo(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(100))
-#     complete = db.Column(db.Boolean)
+# Definir o modelo Todo
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
 
-# Criar as tabelas no banco de dados (comentado para não tentar criar tabelas)
-# with app.app_context():
-#     db.create_all()
+# Criar as tabelas no banco de dados
+with app.app_context():
+    db.create_all()
 
-# Rotas da aplicação (sem interação com o banco de dados)
+# Rotas da aplicação
 @app.route('/')
 def home():
-    todo_list = []  # Criar uma lista vazia para renderizar a interface sem dados
-    return render_template("base.html", todo_list=todo_list, rds_endpoint=rds_endpoint)
+    todo_list = Todo.query.all()
+    return render_template("base.html", todo_list=todo_list)
 
 @app.route("/add", methods=["POST"])
 def add():
-    # Tente adicionar um item à lista sem interagir com o banco de dados
     title = request.form.get("title")
-    # Simule a adição de um item
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
     return redirect(url_for("home"))
 
 @app.route("/update/<int:todo_id>")
 def update(todo_id):
-    # Simular a atualização do item sem interagir com o banco de dados
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
     return redirect(url_for("home"))
 
 @app.route("/delete/<int:todo_id>")
 def delete(todo_id):
-    # Simular a exclusão do item sem interagir com o banco de dados
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
     return redirect(url_for("home"))
 
 # Função para inicializar o app
 def create_app():
-    # Comentar a criação das tabelas para o teste
-    # with app.app_context():
-    #     db.create_all()
+    with app.app_context():
+        db.create_all()
     return app
